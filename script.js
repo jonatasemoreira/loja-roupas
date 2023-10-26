8a3a24c5cdc68355b061d0a6455385cd6f57c671
Jonatas Elieser Moreira <jonatas.em20@gmail.com>
2023-10-26 12:41:42 -0300

    atualização responsivo

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 const url = "https://fakestoreapi.com/products";
 const container = document.getElementById("container-main");
+const input = document.getElementById("search");
 
 async function getPost() {
   const response = await fetch(url);
   const data = await response.json();
+  let filteredData = data;
 
-  console.log(data);
-  data.forEach(character => {
-      
+  input.addEventListener("input", (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    filteredData = data.filter((product) => {
+      return product.title.toLowerCase().includes(searchTerm);
+    });
+
+    renderProducts(filteredData);
+  });
+
+  renderProducts(data);
+
+  return data;
+}
+
+function renderProducts(data) {
+  container.innerHTML = "";
+
+  data.forEach((product) => {
     const div = document.createElement("div");
     div.classList.add("container-produtos");
 
     const imageElement = document.createElement("img");
     const cateElemente = document.createElement("h3");
     const precoElemento = document.createElement("p");
-    //const  = document.createElement("");
 
-    imageElement.src = character.image;
-    cateElemente.textContent = character.title;
-    precoElemento.textContent = character.price;
+    imageElement.src = product.image;
+    cateElemente.textContent = product.title;
+    precoElemento.textContent = product.price;
 
     div.appendChild(cateElemente);
     div.appendChild(precoElemento);
@@ -26,8 +42,6 @@ async function getPost() {
 
     container.appendChild(div);
   });
-
-  return data;
 }
 
 getPost();
